c852cffd35078f9a3974d518e5068c1f5672e48b
Pavel <gpv86x@gmail.com>
2022-04-29 10:13:36 +0300

    Карточка товара краткая, пагинация, привязка к реакту

--- resources/js/components/CardsBlock.jsx
+++ resources/js/components/CardsBlock.jsx
@@ -1,28 +1,50 @@
 import React from "react";
 import CardTravelSimple from '../components/CardTravelSimple'
-import Grid from '@mui/material/Grid';
 import { useState, useEffect } from 'react';
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination"
 
 const CardsBlock = () => {
-       const [travels, setTravels] = useState([]);
+    const [travels, setTravels] = useState([]);
+    const [page, setPage] = useState(1)
+    
+//   useEffect(() => {
+//         setTravels([{ id:1, city: 'Vologda', type: 'Bus tour', duration: 8, dateNextTtavel: "1 августа 2022", raiting: 1.5, price:"300$", imgSrc:"../../img/img_travel_01.jpg" },
+//           { id: 2, city: 'N.Novgorod', type: 'Bus tour', duration: 48, dateNextTtavel: "1 августа 2022", raiting: 4, price:"30$", imgSrc:"../../img/img_travel_01.jpg"},
+//         { id: 3, city: 'Kiev', type: 'Backpack tour', duration: 4, dateNextTtavel: "1 июля 2022", raiting: 3, price:"320$", imgSrc:"../../img/img_travel_01.jpg" },
+//         { id: 4, city: 'Murmansk', type: 'Canoe tour', duration: 5, dateNextTtavel: "18 июня 2022", raiting: 4, price:"33.99$", imgSrc:"../../img/img_travel_01.jpg" },
+//         { id: 5, city: 'Vologda', type: 'Bike tour', duration: 24, dateNextTtavel: "30 мая 2022", raiting: 4.5, price:"100$", imgSrc:"../../img/img_travel_01.jpg" }]);
+//     },[])
   
-  useEffect(() => {
-     
-        setTravels([{ id:1, city: 'Vologda', type: 'Bus tour', duration: 48 },
-          { id: 2, city: 'N.Novgorod', type: 'Bus tour', duration: 48, dateNextTtavel: "1 августа 2022", imgSrc:"img_travel_01", raiting: 4},
-        { id: 3, city: 'Kiev', type: 'Bus tour', duration: 48, dateNextTtavel: "1 июля 2022" },
-        { id: 4, city: 'Murmansk', type: 'Bus tour', duration: 48, dateNextTtavel: "18 июня 2022" },
-        { id: 5, city: 'Vologda', type: 'Bus tour', duration: 48, dateNextTtavel: "30 мая 2022" }]);
-    },[])
-  
-    return (
-        <Grid container spacing={3}>
 
-            {travels.map(travel => <Grid item sm={6} md={4} ><CardTravelSimple travels={travel} /></Grid>
-                
-                 )}
-        </Grid>
+const useEffect(() => {
+        fetch(`/${page}`)
+             .then(res => res.json())
+             .then(data => setTravels(data))
+    }, [page]);
+   
+    
+    return (
+        <div className="slider">
+            <div className="slider_container">
+                <Swiper
+                    slidesPerView={3}
+                    spaceBetween={30}
+                    modules={[Navigation, Pagination]}
+                    pagination={{
+                    clickable: true,
+                    }}
+                    className="mySwiper">
+                    {travels.map(travel => <SwiperSlide  key={travel.id} ><CardTravelSimple travel={travel}/></SwiperSlide>)}
+                </Swiper>   
+            </div>
+            {/* <div className="slider_btn_container">
+                <div className="slider_btn">before</div>
+                <div className="slider_btn">after</div>
+            </div> */}
+        </div>
     )
 }
 
