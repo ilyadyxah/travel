4294b0211ff296d9e3eb6e6c54bce34cd06cd9fb
Pavel <gpv86x@gmail.com>
2022-05-12 18:19:19 +0300

    Закомьентировал поиск на главной
    Нужно будет туда передать параметры на поиск

--- public/js/app.js
+++ public/js/app.js
@@ -18195,40 +18195,21 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var _Switcher__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./Switcher */ "./resources/js/components/Switcher.jsx");
 /* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
-function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
-
-function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
-
-function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
-
-function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
-
-function _iterableToArrayLimit(arr, i) { var _i = arr == null ? null : typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"]; if (_i == null) return; var _arr = []; var _n = true; var _d = false; var _s, _e; try { for (_i = _i.call(arr); !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
-
-function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
-
 
 
 
 
 
 function Finder(props) {
-  var _useState = (0,react__WEBPACK_IMPORTED_MODULE_0__.useState)(props),
-      _useState2 = _slicedToArray(_useState, 2),
-      findOption = _useState2[0],
-      setFindOption = _useState2[1]; //  console.log(props)
-
-
+  // const [findOption, setFindOption] = useState(props);
+  //  console.log(props)
   return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)("div", {
     className: "finder",
     children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsxs)("form", {
       children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)("h3", {
         children: "\u041F\u043E\u0438\u0441\u043A \u043F\u0443\u0442\u0435\u0448\u0435\u0441\u0442\u0432\u0438\u044F"
-      }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsxs)("div", {
-        className: "finder__switchers",
-        children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)(_Switcher__WEBPACK_IMPORTED_MODULE_1__["default"], {
-          props: findOption
-        }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)(_Switcher__WEBPACK_IMPORTED_MODULE_1__["default"], {}), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)(_Switcher__WEBPACK_IMPORTED_MODULE_1__["default"], {}), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)(_Switcher__WEBPACK_IMPORTED_MODULE_1__["default"], {})]
+      }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)("div", {
+        className: "finder__switchers"
       }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)("p", {
         children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)("input", {
           className: "btn finder_btn",
--- resources/js/components/Finder.jsx
+++ resources/js/components/Finder.jsx
@@ -3,7 +3,7 @@ import Switcher from './Switcher'
 import { useState } from 'react';
 
 export default function Finder(props) {
-  const [findOption, setFindOption] = useState(props);
+  // const [findOption, setFindOption] = useState(props);
 
   //  console.log(props)
   return (
@@ -11,10 +11,10 @@ export default function Finder(props) {
       <form>
         <h3>Поиск путешествия</h3>
         <div className='finder__switchers'>
-          <Switcher props={findOption} />
+          {/* <Switcher props={findOption} /> */}
+          {/* <Switcher />
           <Switcher />
-          <Switcher />
-          <Switcher />
+          <Switcher /> */}
         </div>
 
         <p><input className='btn finder_btn' type="submit" value="Найти путешествие" /></p>
