24057e34e84421c3c37af573ef51688a74cfbcd8
Pavel <gpv86x@gmail.com>
2022-06-03 16:35:06 +0300

    Стили карточек отзывов, страницы путешествия

--- resources/views/place.blade.php
+++ resources/views/place.blade.php
@@ -111,36 +111,38 @@
         <div class="col-6 h-100">
             <div class="bg-light me-md-3 pt-1 px-1 pt-md-1 px-md-1 text-center">
                 <div class=" p-3">
-                        <div class="place_box">
-                        <h2 class="display-5" data-id="city_name">@foreach($place->cities as $city){{ Str::ucfirst($city->title) }}@endforeach</h2>
-                        <div class="like_box">
-                            <span like="{{$place->id}}" onclick="likeHandle(this)">
-                                @if(in_array($place->id, $likes))
-                                    <i class="fa-solid fa-thumbs-up"></i>
-                                @else
-                                    <i class="fa-regular fa-thumbs-up"></i>
-                                @endif
-                            </span>
-                            <span id="like-{{$place->id}}" class="">{{ $place->likes->count() === 0 ? '' : $place->likes->count() }}</span>
-                            @auth
-                            <span favorite="{{$place->id}}" id="favorite-{{ $place->id }}" onclick="favoriteHandle(this)">
-                                @if(in_array($place->id, $favorites))
-                                    <i class="fa-star fa-solid"></i>
-                                @else
-                                    <i class="fa-star fa-regular"></i>
-                                @endif
-                            </span>
-                            @endauth
+                    <div class="place_content">
+                            <div class="place_box">
+                            <h2 class="display-5" data-id="city_name">@foreach($place->cities as $city){{ Str::ucfirst($city->title) }}@endforeach</h2>
+                            <div class="like_box">
+                                <span like="{{$place->id}}" onclick="likeHandle(this)">
+                                    @if(in_array($place->id, $likes))
+                                        <i class="fa-solid fa-thumbs-up"></i>
+                                    @else
+                                        <i class="fa-regular fa-thumbs-up"></i>
+                                    @endif
+                                </span>
+                                <span id="like-{{$place->id}}" class="">{{ $place->likes->count() === 0 ? '' : $place->likes->count() }}</span>
+                                @auth
+                                <span favorite="{{$place->id}}" id="favorite-{{ $place->id }}" onclick="favoriteHandle(this)">
+                                    @if(in_array($place->id, $favorites))
+                                        <i class="fa-star fa-solid"></i>
+                                    @else
+                                        <i class="fa-star fa-regular"></i>
+                                    @endif
+                                </span>
+                                @endauth
+                            </div>
                         </div>
-                    </div>
-                    <div class="place_description">
-                        <p class="text_description">{{ $place->description }}</p>
-                        <p class="text_description"><span class="">Расстояние от города: </span>{{ $place->distance }} км</p>
-                        <p class="text_description"><span>Сложность: </span>{{ $place->complexity }} из 100</p>
-                        <p class="text_description"><span>Как добраться: </span>@foreach($place->transports as $transport){{ Str::ucfirst($transport->title) }} @endforeach</p>
-                        <p class="text_description"><span>Сколько стоит: </span>{{ $place->cost ? $place->cost . 'руб' : 'бесплатно'}} </p>
-                        <p id="route_description"></p>
+                        <div class="place_description">
+                            <p class="text_description">{{ $place->description }}</p>
+                            <p class="text_description"><span class="">Расстояние от города: </span>{{ $place->distance }} км</p>
+                            <p class="text_description"><span>Сложность: </span>{{ $place->complexity }} из 100</p>
+                            <p class="text_description"><span>Как добраться: </span>@foreach($place->transports as $transport){{ Str::ucfirst($transport->title) }} @endforeach</p>
+                            <p class="text_description"><span>Сколько стоит: </span>{{ $place->cost ? $place->cost . 'руб' : 'бесплатно'}} </p>
+                            <p id="route_description"></p>
 
+                        </div>
                     </div>
                     <div class="route">
                         <div class="route_coordinates">
@@ -165,7 +167,14 @@
             </div>
         </div>
     </div>
-
+    
+    <div class="row row-cols-1 row-cols-md-2 g-4">
+        @include('components/review')
+        @include('components/review')
+        @include('components/review')
+        @include('components/review')
+        @include('components/review')
+    </div>
 
     <!-- Swiper JS -->
     <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
