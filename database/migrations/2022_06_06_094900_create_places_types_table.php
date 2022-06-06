d3c87ae7d90cad6d54a11f6aabe6f85c3c44fc63
Nikolay <nikolay.vanzhin@yandex.ru>
2022-06-06 15:13:03 +0500

    db updated for parse: groups table, types table created

---
+++ database/migrations/2022_06_06_094900_create_places_types_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('places_types', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('type_id');
+            $table->unsignedBigInteger('place_id');
+            $table->foreign('type_id')
+                ->references('id')
+                ->on('types')
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate();
+            $table->foreign('place_id')
+                ->references('id')
+                ->on('places')
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('places_types', function (Blueprint $table) {
+            $table->dropForeign(['type_id', 'place_id']);
+            $table->dropIfExists('places_groups');
+        });    }
+};
