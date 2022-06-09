469e50462ba50e88e0da9dd829b974c07bbcf02b
Nikolay <nikolay.vanzhin@yandex.ru>
2022-06-10 00:04:48 +0500

    parseController update, db update

---
+++ app/Http/Requests/Source/CreateRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Source;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'count' => ['required', 'integer', 'max:10'],
+            'source-id' => ['required'],
+
+        ];
+    }
+}
